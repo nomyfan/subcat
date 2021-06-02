575cb12b0bce6c178b21e6892aa86ed74e94e316
nomyfan <Nomyfan@live.com>
2021-06-02 15:03:44 +0800

    feat: cli

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use clap::{App, Arg};
 use image::{GenericImage, ImageBuffer, RgbaImage};
 use serde::{Deserialize, Serialize};
 use std::io::Read;
@@ -21,9 +22,8 @@ struct Config {
     save_to: String,
 }
 
-fn load_config(config_path: Option<String>) -> Result<Config> {
-    let path = config_path.unwrap_or_else(|| "./config.json".into());
-    let mut config_file = std::fs::File::open(path)?;
+fn load_config(config_path: &str) -> Result<Config> {
+    let mut config_file = std::fs::File::open(config_path)?;
     let mut raw_config = String::new();
     config_file.read_to_string(&mut raw_config)?;
 
@@ -32,8 +32,26 @@ fn load_config(config_path: Option<String>) -> Result<Config> {
     Ok(config)
 }
 
+fn cli<'a, 'b>() -> App<'a, 'b> {
+    App::new("subcat")
+        .author("Kim Chan(@nomyfan)")
+        .version("0.1.0")
+        .about("A utility to concatenate subtitles")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Config file")
+                .default_value("./config.json"),
+        )
+}
+
 fn main() -> Result<()> {
-    let config = load_config(None)?;
+    let app = cli();
+    let matches = app.get_matches();
+
+    let config = load_config(matches.value_of("config").unwrap())?;
     if config.imgs.is_empty() {
         eprintln!("No images specified, please check the config file.");
         std::process::exit(1);
@@ -42,6 +60,7 @@ fn main() -> Result<()> {
     let mut final_height = 0u32;
     let mut final_width = 0u32;
     let mut images = vec![];
+    println!("Loading images...");
     for img_config in &config.imgs {
         let mut img = image::open(img_config.path.to_string()).unwrap();
 
@@ -60,11 +79,13 @@ fn main() -> Result<()> {
     let mut final_img: RgbaImage = ImageBuffer::new(final_width, final_height);
     let mut y_offset = 0u32;
 
+    println!("Generating...");
     for img in &images {
         final_img.copy_from(img, 0, y_offset)?;
         y_offset += img.dimensions().1;
     }
 
+    println!("Saving...");
     final_img.save(config.save_to)?;
 
     Ok(())
