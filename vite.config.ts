7f83cc5bf09ecc6075d8ef6f0ff8459d9c7fb337
nomyfan <Nomyfan@live.com>
2023-02-05 23:43:11 +0800

    chore(vite): fix dev config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    drop: ["debugger", "console"],
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    esbuild:
+      mode === "development"
+        ? undefined
+        : {
+            drop: ["debugger", "console"],
+          },
+  };
 });
