4b70561ccfd12d3267e3916843cc32287fa0aea0
nomyfan <Nomyfan@live.com>
2023-04-15 21:00:14 +0800

    chore(deps): bump

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
+    server: {
+      port: 3000,
+    },
     esbuild:
       mode === "development"
         ? undefined
