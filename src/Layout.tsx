c91463aad6bc0e5e7201c7dffeab92022c4d3c62
nomyfan <Nomyfan@live.com>
2023-10-25 20:57:12 +0800

    feat: panel seperator

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import { Separator } from "subcat/components/ui/separator";
 
 function Layout(props: {
   middle: ReactNode;
@@ -9,12 +10,13 @@ function Layout(props: {
   return (
     <main className="flex flex-col h-screen select-none overflow-hidden">
       <section className="flex-grow-0">{props.head}</section>
+      <Separator orientation="horizontal" />
 
       <div className="flex-grow flex" style={{ height: "calc(100% - 40px)" }}>
         <section className="w-[300px] overflow-auto">{props.left}</section>
-
+        <Separator orientation="vertical" />
         <section className="grow w-0">{props.middle}</section>
-
+        <Separator orientation="vertical" />
         <section className="w-[800px] overflow-auto pb-10">
           {props.right}
         </section>
